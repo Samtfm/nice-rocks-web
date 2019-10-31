b591c1dfbd49b4a7c0c8138a5528be961daa7adb
Samtfm <sfmemail@gmail.com>
2019-10-30 19:40:32 -0700

    fix port for heroku

--- server.js
+++ server.js
@@ -74,7 +74,8 @@ app.get('/login', (req, res) => {
 });
 
 app.use(express.static('public'));
-app.listen(3000,  () => console.log("Example app listening on port 3000!"));
+var port = process.env.PORT || 3000;
+app.listen(port,  () => console.log("Example app listening on port " + port + "!"));
 
 
 app.post('/sessionLogin', (req, res) => {
