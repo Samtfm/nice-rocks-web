591ff12a22fa073d3bdef41efb146b1ba209a563
Maryana Pinchuk <maryanapinchuk@Maryanas-MacBook-Pro.local>
2019-11-20 21:26:59 -0800

    clean up view rock modal

--- src/components/RockModal.js
+++ src/components/RockModal.js
@@ -2,29 +2,47 @@ import React from 'react';
 import styles from './RockModal.scss';
 import Modal from './Modal';
 
-class RockModal extends React.Component {
-  render() {
+const TitleUrlHeadline = ({rock}) => (
+  <a href={rock.url} target="_blank" className={styles['link-headline']}>
+    <h2>{rock.title}</h2>
+    <div className={styles['open-icon']}>{"Open"}</div>
+  </a>
+)
 
+const UrlHeadline = ({rock}) => (
+  <a href={rock.url} target="_blank" className={styles['link-headline']}>
+    <p className={styles['url']}>{rock.url}</p>
+    <div className={styles['open-icon']}>{"Open"}</div>
+  </a>
+)
+
+const TitleHeadline = ({rock}) => (
+  <h2 className={styles['basic-headline']}>{rock.title}</h2>
+)
+
+
+class RockModal extends React.Component {
+
+  render() {
     const { rock, visible, handleClose } = this.props;
+
+    let HeadlineComp = () => "";
+    if (rock.url && rock.title) {
+      HeadlineComp = TitleUrlHeadline
+    } else if (rock.url) {
+      HeadlineComp = UrlHeadline
+    } else if (rock.title) {
+      HeadlineComp = TitleHeadline
+    }
+
     return  (
       <Modal visible={visible} handleClose={handleClose}>
         <div className={styles['content']}>
-
-          <section className={styles['link-section']}>
-            <a href={rock.url}  >
-              <h2>
-                {rock.title || rock.url}
-              </h2>
-              <icon>O</icon>
-            </a>
-          </section>
+          <HeadlineComp rock={rock}/>
           <section>
             <p>
               {rock.note}
             </p>
-          </section>
-          <section>
-            thing3
           </section>
         </div>
       </Modal>
